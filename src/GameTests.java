76b47c124ca951d9abd2e3f1bf1bd6e1a43896c5
Malachi Swedberg <malachiswedberg@gmail.com>
2019-03-26 14:01:05 -0500

    Fourth Commit

--- src/GameTests.java
+++ src/GameTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class GameTests {
 
+	@SuppressWarnings("serial")
 	@Test
 	public void BoardGameTest(){
 		BoardGame bg = new BoardGame();
@@ -15,5 +16,33 @@ public class GameTests {
 		Assert.assertEquals("Incorrect getPlayerGamePiece Method", GamePiece.BLUE_BOOT, bg.getPlayerGamePeice("jimmy"));
 		Assert.assertEquals("Incorrect getPlayerWithGamePiece Method", "jimmy", bg.getPlayerWithGamePeice(GamePiece.BLUE_BOOT));
 		Assert.assertEquals("Incorrect getPlayersLocation Method", Location.BALLROOM, bg.getPlayersLocation("jimmy"));
+		Assert.assertEquals("Incorrect getPlayersAtLocation Method", new ArrayList<String>() {
+			{	
+				add("jimmy");
+				add("joe");
+			}
+		}, bg.getPlayersAtLocation(Location.BALLROOM));
+		
+		Assert.assertEquals("Incorrect getGamePiecesAtLocation Method", new ArrayList<GamePiece>() {
+			{
+				add(GamePiece.BLUE_BOOT);
+				add(GamePiece.BLUE_RACER);
+			}
+		}, bg.getGamePeicesAtLocation(Location.BALLROOM));
+		
+		Assert.assertEquals("Incorrect getPlayers Method", new LinkedHashSet<String>() {
+			{
+				add("jimmy");
+				add("joe");
+				add("josh");
+			}
+		}, bg.getPlayers());
+		
+		Assert.assertEquals("Incorrect getPlayersLocations Method", new LinkedHashSet<Location>() {
+			{
+				add(Location.BALLROOM);
+				add(Location.BILLIARD_ROOM);
+			}
+		}, bg.getPlayersLocations());
 	}
 }
